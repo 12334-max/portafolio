7785328415f8b8c26a98470f0bc9513fd8d23b87
Javi Perez <75823630+12334-max@users.noreply.github.com>
2024-07-08 22:36:49 -0600

    Update Presentation.jsx

--- src/components/profile/Presentation.jsx
+++ src/components/profile/Presentation.jsx
@@ -4,9 +4,8 @@ function Presentation() {
         <div className='rounded-xl p-4 mb-10 border w-auto md:w-60 lg:w-auto'>
             <p className='text-zinc-400 text-center hover:-translate-y-1 hover:scale-100 duration-300'>
                 Soy desarrollador de software con experiencia en el
-                desarrollo web con laravel y competente en el desarrollo
-                móvil con flutter y muy apasionado en aprender nuevas
-                tecnologías
+                desarrollo web con frameworks de AngularTS, APIs en C# ASP.NET y en el desarrollo móvil en 
+                kotlin con jectpack Compose
             </p>
         </div>
     )
